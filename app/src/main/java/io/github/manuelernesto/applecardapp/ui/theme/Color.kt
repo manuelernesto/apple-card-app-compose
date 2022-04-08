aa17bb13efbecf7f927828969293af17021ca877
manuelernesto <alemdadox@gmail.com>
2022-04-08 19:43:44 +0100

    🚀 setup the colors

--- app/src/main/java/io/github/manuelernesto/applecardapp/ui/theme/Color.kt
+++ app/src/main/java/io/github/manuelernesto/applecardapp/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val Orange = Color(0xFFfbac00)
+val Blue = Color(0xFF3496c9)
+val Dark = Color(0xFF1e1e1f)
+val Green = Color(0xFF50be3c)
+val Red = Color(0xFFf26d5f)
+val White = Color(0xFFffffff)
+val WhiteBg = Color(0xFFf2f3f8)
